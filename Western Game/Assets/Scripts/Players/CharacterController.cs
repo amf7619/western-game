e7cc9807450611307751559411d241cced7eafc6
Anthony Ferraioli <amf7619@g.rit.edu>
2020-06-29 17:42:32 -0400

    Wrote player movement

--- Western Game/Assets/Scripts/Players/CharacterController.cs
+++ Western Game/Assets/Scripts/Players/CharacterController.cs
@@ -4,6 +4,20 @@ using UnityEngine;
 
 public class CharacterController : MonoBehaviour
 {
+    public bool movementEnabled = true;
+    public bool jumpEnabled = true;
+
+    private float horizontalMove;
+    private float verticalMove;
+    private float jumpMove;
+
+    private Vector3 movement;
+
+    [SerializeField]
+    private float movementSpeed = 5;
+    [SerializeField]
+    private float jumpHeight = 5;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -11,8 +25,41 @@ public class CharacterController : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        movement = Vector3.Lerp(movement, Vector3.zero, 0.5f);
+
+        CalculateMove();
+        CalculateJump();
+        MovePlayer();
+    }
+
+    void CalculateMove()
+    {
+        if (!movementEnabled)
+        {
+            return;
+        }
+
+        horizontalMove = Input.GetAxis("Horizontal");
+        verticalMove = Input.GetAxis("Vertical");
+    }
+
+    void CalculateJump()
+    {
+        if(!jumpEnabled)
+        {
+            return;
+        }
+
+        jumpMove = Input.GetAxisRaw("Jump");
+    }
+
+    void MovePlayer()
+    {
+        movement = new Vector3(horizontalMove, 0, verticalMove).normalized * movementSpeed;
+        movement.y += jumpMove * jumpHeight;
+
+        transform.position += movement * Time.deltaTime;
     }
 }
